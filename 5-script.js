e0a57121bdf7ce9a2cd912f7ec2f976eae916777
chennaiahgari-kavya <ckavyar@gmail.com>
2025-05-22 20:25:45 +0100

    new

--- 5-script.js
+++ 5-script.js
@@ -5,19 +5,20 @@
     //block-1 and scope-1
     {
 
-        function block2() {
             let a3 = ["car", "bike", "scooty", "lorry", "train"];
-            console.log("BLOCK2", a3[2]);
-        } block2();
+            console.log("BLOCK2", a3);
+            console.log("BLOCK1", a1);
+        
         //block-2 and scope-2
+         
         {
             //block-3 and scope-3
-            function block1() {
+            
                 let a2 = ["minnu", "Sree", "kranthi", "jassu", "kavya"];
-                console.log(a2[2]);
-                console.log("BLOCK3", a1);
-            }
-            block1();
+                console.log("BLOCK3", a2);
+                console.log("BLOCK1", a1);
+                console.log("BLOCK2", a3);
+            
         }
     }
 }
