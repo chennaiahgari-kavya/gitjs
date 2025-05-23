cde8d1e85c27ae3a31ad87fb6d3d2600a0c7cea4
chennaiahgari-kavya <ckavyar@gmail.com>
2025-05-23 14:00:25 +0100

    debugging

---
+++ 6-script.js
@@ -0,0 +1,16 @@
+let foodItem="pizza";
+console.log(foodItem);
+
+
+debugger;
+let deliveryLocation="hyd";
+console.log(deliveryLocation);
+
+
+debugger;
+let estimationTime="30min";
+console.log(estimationTime);
+
+debugger;
+let isDeliveryAvaliable="yes";
+console.log(isDeliveryAvaliable);
---
+++ 7-script.js
@@ -0,0 +1,19 @@
+function block1()
+{
+    let a2=["minnu","Sree","kranthi","jassu","kavya"];
+    console.log(a2[2]);
+    console.log("ids of students");
+}
+
+let foodItem="pizza";
+console.log(foodItem);
+
+
+
+let deliveryLocation="hyd";
+console.log(deliveryLocation);
+function kavya(){
+    block1();
+    console.log("hello world")
+}
+kavya();
